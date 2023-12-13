00079eefc530adc6c4493145a4be01d3f0df5645
jungyunmok <jungyunmok@gmail.com>
2023-12-14 00:15:10 +0900

    feat: 방문 날짜, 주문 메뉴 입력 기능

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,19 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    // 고객이 식당에 방문할 날짜 입력받기
+    public String readDate() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String input = Console.readLine();
+        return input;
+    }
+
+    public String readMenu() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = Console.readLine();
+        return input;
+    }
 }
