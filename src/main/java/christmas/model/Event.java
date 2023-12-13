506993ea1e84fa86cbf987416a8f494b50c25c96
jungyunmok <jungyunmok@gmail.com>
2023-12-13 22:11:31 +0900

    feat: 크리스마스 디데이 할인 기능

--- src/main/java/christmas/model/Event.java
+++ src/main/java/christmas/model/Event.java
@@ -1,4 +1,13 @@
 package christmas.model;
 
 public class Event {
+    // 크리스마스 디데이 할인
+    public int xMas(int date) {
+        int sale = 0;
+        if (date <= 25) {
+            sale = 1000 + (100 * (date - 1));
+        }
+        return sale;
+    }
+
 }
