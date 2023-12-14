207a38c024dee564c7a5bfe52c0dfaad1db892e9
jungyunmok <jungyunmok@gmail.com>
2023-12-14 15:56:48 +0900

    refactor: 컨트롤러 분할

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -5,6 +5,6 @@ import christmas.controller.MainController;
 public class Application {
     public static void main(String[] args) {
         MainController mainController = new MainController();
-        mainController.getDate();
+        mainController.startOrder();
     }
 }
--- src/main/java/christmas/controller/MainController.java
+++ src/main/java/christmas/controller/MainController.java
@@ -8,6 +8,7 @@ import christmas.model.Result;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MainController {
@@ -26,35 +27,51 @@ public class MainController {
         outputView = new OutputView();
     }
 
+    // 주문 시작
+    public void startOrder() {
+        int date = getDate();
+        Map<String, Integer> orders = getOrder(date);
+        int totalAmount = getAmount(orders);
+        applySale(date, orders, totalAmount);
+    }
+
     // 날짜 받기
-    public void getDate() {
+    public int getDate() {
+        int date = 0;
         try {
             String strDate = inputView.readDate();
-            int date = judgement.checkInt(strDate);
+            date = judgement.checkInt(strDate);
             judgement.checkRange(XMasInfo.END_DATE, date);
-            getOrder(date);
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
             getDate();
         }
+        return date;
     }
 
     // 주문 받기
-    public void getOrder(int date) {
+    public Map<String, Integer> getOrder(int date) {
+        Map<String, Integer> orders = new HashMap<>();
         try {
             String order = inputView.readMenu();
-            Map<String, Integer> orders = judgement.checkDuplication(order);
+            orders = judgement.checkDuplication(order);
             judgement.checkMenu(orders);
             judgement.checkDrink(orders);
             result = new Result(orders);
-            int totalAmount = result.totalAmount();
-            outputView.printMenu(orders);
-            outputView.printTotalAmount(totalAmount);
-            applySale(date, orders, totalAmount);
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
             getOrder(date);
         }
+        return orders;
+    }
+
+    // 총 금액 산출하기
+    public int getAmount(Map<String, Integer> orders) {
+        int totalAmount = 0;
+        totalAmount = result.totalAmount();
+        outputView.printMenu(orders);
+        outputView.printTotalAmount(totalAmount);
+        return totalAmount;
     }
 
     // 이벤트 내역 적용하기
@@ -68,9 +85,7 @@ public class MainController {
         totalSale += judgeSale("특별 할인", event.specialSale(date));
         totalSale += judgeSale("증정 이벤트", gift);
         result.totalSale(totalSale);
-        if (totalSale == 0) {
-            System.out.println("없음");
-        }
+        if (totalSale == 0) System.out.println("없음");
         outputView.printTotalSale(totalSale);
         outputView.printFinalAmount(totalAmount, totalSale - gift);
         outputView.printBadge(event.badge(totalSale));
